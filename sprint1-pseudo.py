d7c8da3246dc5fa778ba2898ca4aa51d502a738b
adleonis <gallet.stephane@gmail.com>
2018-03-12 17:58:11 -0400

    1.0

---
+++ sprint1-pseudo.py
@@ -0,0 +1,32 @@
+''' 
+1. Spin up n nodes
+    create script to generate TOKENS & save public key and privates keys locally for project developers
+    change config on buitlscript to allow chain's ports 
+    run dotfiles builtscript
+    save IP's in config file, define central server & slaves
+
+2. Script to install chain on all nodes
+    - multichain
+    - dependencies
+    - report success/failures to central server
+
+3. script to configure the c-nodes on the chain
+    - create the chain(name, mining difficulty, ...)
+    - create the asset
+    - set the miners activity
+    - set the 
+
+4. Create Variables file which holds all script variables  (thursday/friday)
+
+
+
+
+Definitions:
+
+node = server VM
+chain = blockchain
+c-node = multichain node(running full multichain miner)
+user = multichain address, simulating a person doing transactions
+
+    
+''' 
